f2364ba9a6153ee4f55c1e2599751b9ab379e9cf
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-23 16:17:52 +0300

    ft:added the views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+
+def home(request):
+    context= dict()
+
+    return render(request,'base/home.html',context)
