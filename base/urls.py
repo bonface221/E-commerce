523dec7d11b8b2fb0ebf190fba77bf5c670e5cb2
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-28 07:23:47 +0300

    ft:checkout page

--- base/urls.py
+++ base/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 
 
     path('', views.home, name='home'),
-    path('cart/', views.cart, name='cart')
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
--- base/views.py
+++ base/views.py
@@ -42,6 +42,24 @@ def cart(request):
     
     else :
         items=[]
+        order=dict(get_cart_total=0,get_cart_items=0)
     
-    context = dict(items=items)
+    context = dict(items=items,order=order)
     return render(request,'base/cart.html',context)
+
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order , created =Order.objects.get_or_create(customer=customer,complete= False)
+        items = order.orderitem_set.all()
+        
+    
+    else :
+        items=[]
+        order=dict(get_cart_total=0,get_cart_items=0)
+
+    context = dict(items=items,order=order)
+
+    return render(request,'base/checkout.html',context)
