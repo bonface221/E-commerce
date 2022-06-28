081d824301b6f5f09d0b95634a0c4aa61dde55db
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-28 09:49:10 +0300

    ft:ajax functionality

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render,redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout
@@ -5,6 +6,8 @@ from .forms import NewUserForm
 from django.views.generic.edit import CreateView
 from django.contrib.auth.decorators import login_required
 from .models import *
+from django.http import JsonResponse
+import json
 
 
 # Create your views here.
@@ -63,3 +66,27 @@ def checkout(request):
     context = dict(items=items,order=order)
 
     return render(request,'base/checkout.html',context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('productId',productId)
+    print('Action',action)
+
+    customer = request.user.customer
+
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem,created =OrderItem.objects.get_or_create(order=order,product=product)
+
+    if action =='add':
+        orderItem.quantity = (orderItem + 1)
+
+    elif action == 'remove':
+        orderItem.quantity = (orderItem -1)
+    
+    orderItem.save()
+    return JsonResponse('Item was added' , safe=False)
